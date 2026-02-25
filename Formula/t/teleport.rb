ca83325454514ed466cc744752caeaa4d85b1f6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-25 18:00:11 +0000

    teleport 18.7.1

--- Formula/t/teleport.rb
+++ Formula/t/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://goteleport.com/"
-  url "https://github.com/gravitational/teleport/archive/refs/tags/v18.7.0.tar.gz"
-  sha256 "a57250671a1879498c629a0f6c06774c62423acf40f193bef438cb0384e75c93"
+  url "https://github.com/gravitational/teleport/archive/refs/tags/v18.7.1.tar.gz"
+  sha256 "6e783e34859e152a05918e9254b91779a7f001ddaad0b2ad86f41193cf0c62b9"
   license all_of: ["AGPL-3.0-or-later", "Apache-2.0"]
   head "https://github.com/gravitational/teleport.git", branch: "master"
 
@@ -47,8 +47,8 @@ class Teleport < Formula
   conflicts_with cask: "tsh", because: "both install `tsh` binaries"
 
   resource "wasm-bindgen" do
-    url "https://github.com/wasm-bindgen/wasm-bindgen/archive/refs/tags/0.2.99.tar.gz"
-    sha256 "1df06317203c9049752e55e59aee878774c88805cc6196630e514fa747f921f2"
+    url "https://github.com/wasm-bindgen/wasm-bindgen/archive/refs/tags/0.2.113.tar.gz"
+    sha256 "ee2183913e2d92f20f60a480343b23eff043090e250393ca31769b8337b03084"
   end
 
   # disable `wasm-opt` for ironrdp pkg release build, upstream pr ref, https://github.com/gravitational/teleport/pull/50178
